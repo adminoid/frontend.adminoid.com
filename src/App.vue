bb87b92ac519b7ad59948b2462f0b5d19a830d25
adminoid <mr@adminoid.com>
2016-12-26 14:14:03 +0300

    Clean installation with jquery and semantic-ui

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ export default {
 </script>
 
 <style>
+@import '../semantic/dist/semantic.min.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
