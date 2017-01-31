4302996346e444ace1e88064445ff05d2f603040
adminoid <mr@adminoid.com>
2017-01-31 11:00:12 +0300

    Process layout rebuild completed

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    // index: path.resolve(__dirname, '../dist/index.html'),
-    index: path.resolve(__dirname, '../dist/resources/views/index.blade.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    // index: path.resolve(__dirname, '../dist/resources/views/index.blade.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
