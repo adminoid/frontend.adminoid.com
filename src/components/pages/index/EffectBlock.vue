daa6033e46f9aad6452d4859e6b0a8eaedc14f08
Petr Zenin <mr@adminoid.com>
2017-07-24 16:10:57 +0300

    complete index page as is

--- src/components/pages/index/EffectBlock.vue
+++ src/components/pages/index/EffectBlock.vue
@@ -15,3 +15,16 @@
       }
     }
 </script>
+<template>
+  <div class="ui container effect-block" v-bind:class="getClass()" v-on:click="toggleClass">
+    <img class="ui fluid image" src="static/img/adminoid/pages/main/reliability.jpg">
+    <div class="inner">
+      <div class="text">
+        <p>За годы работы (с июня 2008) на крупнейшей российской бирже фриланса FL.ru у меня только положительные отзывы
+          и завершенные сделки (см. отзывы).
+        <p>В начале пути, когда я был неопытен, шанс совершить ошибку был еще велик, но я им не воспользовался.</p>
+        <p>А как теперь накосячить ума не приложу…</p>
+      </div>
+    </div>
+  </div>
+</template>
