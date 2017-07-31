4b262c8ebb74b26e790b5bdeefac6d71b455883a
Petr Zenin <mr@adminoid.com>
2017-07-31 12:57:25 +0300

    Middle work that gulp semantic

---
+++ docs/errors/preserve-comments.md
@@ -0,0 +1,5 @@
+If error is:
+Caused by: DefaultsError: `preserveComments` is not a supported option
+
+In file: semantic/tasks/config/tasks.js
+comment string preserveComments
--- src/components/Styles.vue
+++ src/components/Styles.vue
@@ -1,1 +1,7 @@
-<style src="../../semantic/dist/semantic.min.css"></style>
+<style>
+  @import '../../semantic/dist/components/reset.min.css';
+  @import '../../semantic/dist/components/site.min.css';
+  @import '../../semantic/dist/components/button.min.css';
+  @import '../../semantic/dist/components/container.min.css';
+  @import '../../semantic/dist/semantic.min.css';
+</style>
